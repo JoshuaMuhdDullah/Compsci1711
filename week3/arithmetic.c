8f548f97af0108f0f494751c3410819a290831e2
JoshuaMuhdDullah <146445696+JoshuaMuhdDullah@users.noreply.github.com>
2023-10-23 13:39:03 +0000

    arithmetic

---
+++ week3/arithmetic.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+float arithmetic(float num1, char operation, float num2)
+{
+    float calculation;
+    switch (operation)
+    {
+        case '+':
+            calculation = num1 + num2;
+            printf("%f + %f = %f\n",num1, num2, calculation);
+            break;
+
+        case '-':
+            calculation = num1 - num2;
+            printf("%f - %f = %f\n",num1, num2, calculation);
+            break;
+
+        case '*':
+            calculation = num1 * num2;
+            printf("%f x %f = %f\n",num1, num2, calculation);
+            break;
+            
+        case '/':
+            calculation = num1/num2;
+            printf("%f / %f = %f\n",num1, num2, calculation);
+            break;
+
+        default:
+            printf("Invalid operation\n");
+    }
+    return calculation;
+}
+
+void main() 
+{
+    arithmetic(2.1,'*',2.4);
+}
